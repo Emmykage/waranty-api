53dfae599803bfe6621de37aa4d93c53c9c5dc70
morris menanya <emmiemenz@gmail.com>
2025-10-02 00:24:17 +0100

    configure hybrid db connection

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -4,19 +4,26 @@ import { Pool } from "pg";
 dotenv.config()
 console.log("[DB PASSWORDs]", process.env.DB_PASSWORD)
 
-const pool = new Pool({
-    // user: process.env.USER,
-    // host: process.env.HOST,
-    // database: process.env.DATABASE,
-    // password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT
-  connectionString: process.env.DATABASE_URL,
+
+let poolConfig 
+
+if(process.env.DATABASE_URL){
+poolConfig = {connectionString: process.env.DATABASE_URL,
    ssl: {
     rejectUnauthorized: false, // required for Render
-  },
+  },}
+}else{
 
+    poolConfig={
+     user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+    }
+}
 
-})
+const pool = new Pool(poolConfig)
 
 pool.on("connect", ()=> {
     console.log("Conection pool established with db")
